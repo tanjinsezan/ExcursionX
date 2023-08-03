bee88ae124e3ed0eca7965011f515c41e37794b7
Tanjin Sezan <tanjinsizan51@gmail.com.com>
2023-08-04 00:07:43 +0600

    Initial Commit

---
+++ ExcursionX/ExcursionXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExcursionXApp.swift
+//  ExcursionX
+//
+//  Created by Tanjin Rashid Sezan on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ExcursionXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
